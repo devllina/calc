f3f852a704d592c87e15922576b1e55faf7627a6
Adelina <adelinatokonova232@gmail.com>
2025-11-28 16:12:59 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// const calc = ()  => {
+//     alert(10+30)
+// }
+// calc()
+
+// аргумент/параметр-сырттан келе турчу кошумча маалымат.Ар дайым озгоруп турат
+//  const baryy = (go)  => {
+//     console.log(go)
+
+//  }
+//  baryy('Кинотеатр')
+//  baryy('Универ')
+//  baryy('Колледж')
+     
+//   const calcPlus = (sum1,sum2)  => {
+//        console.log(sum1+sum2)
+
+//   }
+//   calcPlus(10,40)
+
+//   const calcMinus = (sum1,sum2) => {
+//       console.log(sum1-sum2)
+//   }
+//   calcMinus (30,10)
+
+//   const calcMult = (sum1,sum2) => {
+//       console.log(sum1*sum2)
+//   }
+//   calcMult (10,50) 
+
+//   const calcDiv = (sum1,sum2) => {
+//       console.log(sum1/sum2)
+//   }
+//   calcDiv (30,10 )
+
+
+
+  let sum1 = prompt('Биринчи сан')
+  let sum2 = prompt('Экинчи сан')
+
+
+    const calcMinus = (sum1,sum2) => {
+        alert(sum1-sum2)
+
+    }
+
+ 
+    calcMinus(sum1,sum2)
+  
